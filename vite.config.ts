876992b9a5e4504434ebb18ecb8494a1b93a7ef6
mgyang53 <mg960503@gmail.com>
2025-08-27 18:33:42 +0900

    chore: 경로 alias 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,11 @@ export default defineConfig({
     port: 3000,
     open: true,
   },
+  resolve: {
+    alias: {
+      "@": new URL("./src", import.meta.url).pathname,
+      "@components": new URL("./src/components", import.meta.url).pathname,
+      "@utils": new URL("./src/utils", import.meta.url).pathname,
+    },
+  },
 });
